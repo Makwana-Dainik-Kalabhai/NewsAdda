c2f4ff9fb5911b03902418b1c364c5914164819e
Makwana Dainik  Kalabhai <dainikmakwana31@gmail.com>
2024-11-29 19:57:21 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -35,7 +35,7 @@ class App extends Component {
           {/* //* Define unique keys to refresh page & modify data in fetch api */}
           <Route
             exact
-            path="/"
+            path="/NewsAdda/"
             element={
               <News
                 key="general"
@@ -46,7 +46,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/business"
+            path="/NewsAdda/business"
             element={
               <News
                 key="business"
@@ -57,7 +57,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/entertainment"
+            path="/NewsAdda/entertainment"
             element={
               <News
                 key="entertainment"
@@ -68,7 +68,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/health"
+            path="/NewsAdda/health"
             element={
               <News
                 key="health"
@@ -79,7 +79,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/science"
+            path="/NewsAdda/science"
             element={
               <News
                 key="science"
@@ -90,7 +90,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/sports"
+            path="/NewsAdda/sports"
             element={
               <News
                 key="sports"
@@ -101,7 +101,7 @@ class App extends Component {
           ></Route>
           <Route
             exact
-            path="/technology"
+            path="/NewsAdda/technology"
             element={
               <News
                 key="technology"
